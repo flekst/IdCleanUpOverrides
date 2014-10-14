6bbd0113f7926f401be74a8ece9da57e5a218dc8
Eugeny Borisov <flekst@gmail.com>
2014-10-14 11:19:56 +0400

    Initial version. All Right

---
+++ CleanUpOverrides.jsx
@@ -0,0 +1,18 @@
+﻿#target "Indesign"
+/**
+ Сбрасывает изменения стилей для выбранных текстовых блоков
+ (c) 2014 Евгений Борисов .
+ */
+
+try {
+    var mySel = app.activeDocument.selection;
+    var l = mySel.length;
+ } catch (err) { exit(); }   
+ 
+ var i=0;
+ while (i++ < l ) {
+     try {
+        mySel[i].paragraphs.everyItem().clearOverrides(OverrideType.ALL);
+      } catch (err) {}
+}
+
